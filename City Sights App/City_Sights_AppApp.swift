ab905b792fc09629f683eb50caa2f8ee02680180
bodybri <bryanhuang404@gmail.com>
2023-03-23 17:33:05 -0700

    Initial Commit

---
+++ City Sights App/City_Sights_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  City_Sights_AppApp.swift
+//  City Sights App
+//
+//  Created by Bryan Huang on 3/23/23.
+//
+
+import SwiftUI
+
+@main
+struct City_Sights_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
